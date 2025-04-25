6fc9038ec3136b7a1da38d3fc98ec07f50941c84
Franthan <francescovega7@gmail.com>
2025-04-25 15:00:09 -0500

    Agregando mensaje de gestión de usuarios en Main

--- OneDrive/Escritorio/Franthan/Franthan/src/franthan/Franthan.java
+++ OneDrive/Escritorio/Franthan/Franthan/src/franthan/Franthan.java
@@ -14,7 +14,7 @@ public class Franthan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Gestión de usuarios en progreso...");
     }
     
 }
